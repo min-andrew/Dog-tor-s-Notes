42bd88e788f1667543c559c52fc037c6689eb8e9
Andrew Min <minandrew0711@gmail.com>
2023-02-05 00:37:37 -0800

    changed the landing page a bit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,33 @@
 import React from "react";
-
+import FeatureList from "../components/FeatureList";
 import Habit from "../components/HabitsForm";
 import Login from "../components/Login/Login";
 import Signup from "../components/Signup/Signup";
+import Auth from "../utils/auth";
+
 
 const Home = () => {
-  return (
-    <main>
-      <div className="">
-        <div className="">
-          <div>
-            <Login />
-            <Signup />
-          </div>
-          <div>
-            <Habit />
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <div className="">
+                <div className="">
+                    {Auth.loggedIn() ? (
+                        <div>
+                            <Habit />
+                            <FeatureList
+                                title="Summary"
+                            />
+                        </div>
+                    ) : (
+                        <div>
+                            <Login />
+                            <Signup />
+                        </div>
+                    )}
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Home;
