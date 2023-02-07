231b55be6164d0460a6d4680a1cadbe86c897e11
Kenny Wai <97656734+KennyWCoder@users.noreply.github.com>
2023-02-07 00:07:17 -0800

    vetform back to vet note button and adding single profile query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,18 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_SINGLE_PROFILE = gql`
+  query singleProfile($profileId: ID!) {
+    profile(_id: $profileId) {
+      _id
+      petName
+      age
+      breed
+      foodBrand
+      humanName
+    }
+  }  
+`;
+
 export const QUERY_PROFILES = gql`
   query allProfiles {
     profiles {
